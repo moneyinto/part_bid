4e147ed80eb86263d23c4cd10ff04b7d7de7ea6c
your_name <your_email>
2014-07-15 22:52:26 +0800

    竞价结果和统计页面竞价不重复且最小者信息显示

--- app/scripts/controllers/bidding_count.js
+++ app/scripts/controllers/bidding_count.js
@@ -8,37 +8,14 @@ angular.module('card1App')
             'Karma'
         ];
         $scope.back_to_bidding_list = function () {
+            localStorage.removeItem('priceCount');
+            localStorage.removeItem('sucess');
             $location.path('bidding_list');
         };
-
-        var bidList = JSON.parse(localStorage.getItem('bidList'));
-        var bidInformation = bidList[0].bidInformation;
-        var information = _.sortBy(bidInformation, function (num) {
-            return num.bidPrice
-        });
-//        var price = _.groupBy(information,function(num){return num.bidPrice});
-        var priceCount = [];
-        var n = 1;
-        for (var i = 0; i < information.length; i++) {
-            if (i == information.length - 1) {
-                if (information[i].bidPrice == information[i - 1].bidPrice) {
-                    priceCount.push({'bidPrice': information[i].bidPrice, 'count': n});
-                    break;
-                }
-                else {
-                    priceCount.push({'bidPrice': information[i].bidPrice, 'count': n});
-                }
-            }
-            else {
-                if (information[i].bidPrice == information[i + 1].bidPrice) {
-                    n = n + 1;
-                }
-                else {
-                    priceCount.push({'bidPrice': information[i].bidPrice, 'count': n});
-                    n = 1;
-                }
-            }
-        }
+        var priceCount = JSON.parse(localStorage.getItem('priceCount'));
+        var sucess = JSON.parse(localStorage.getItem('sucess'));
         $scope.priceCount = priceCount;
-
+        $scope.sucessName = sucess.name;
+        $scope.sucessPhone = sucess.phone;
+        $scope.sucessPrice = sucess.price
     });
--- app/scripts/controllers/bidding_result.js
+++ app/scripts/controllers/bidding_result.js
@@ -8,6 +8,8 @@ angular.module('card1App')
             'Karma'
         ];
         $scope.back_to_bidding_list = function() {
+            localStorage.removeItem('priceCount');
+            localStorage.removeItem('sucess');
             $location.path('bidding_list');
         };
         var activities = JSON.parse(localStorage.getItem('activities'));
@@ -18,11 +20,42 @@ angular.module('card1App')
         var even = _.find(activities,function(num){return num.name == activityName});
         var peopleList = even.peopleList;
         var people_list = [];
-        for (var i = 0;i < information.length;i++){
-            var list = _.find(peopleList,function(num){return num.personPhone == information[i].bidPhone});
-            people_list.push({'name':list.personName,'phone':list.personPhone,'price':information[i].bidPrice});
+        for (var j = 0;j < information.length;j++){
+            var list = _.find(peopleList,function(num){return num.personPhone == information[j].bidPhone});
+            people_list.push({'name':list.personName,'phone':list.personPhone,'price':information[j].bidPrice});
 
         }
         console.log(people_list[0].name);
         $scope.peopleList = people_list;
+
+        var priceCount = [];
+        var n = 1;
+        for (var i = 0; i < information.length; i++) {
+            if (i == information.length - 1) {
+                if (information[i].bidPrice == information[i - 1].bidPrice) {
+                    priceCount.push({'bidPrice': information[i].bidPrice, 'count': n});
+                    break;
+                }
+                else {
+                    priceCount.push({'bidPrice': information[i].bidPrice, 'count': n});
+                }
+            }
+            else {
+                if (information[i].bidPrice == information[i + 1].bidPrice) {
+                    n = n + 1;
+                }
+                else {
+                    priceCount.push({'bidPrice': information[i].bidPrice, 'count': n});
+                    n = 1;
+                }
+            }
+        }
+        localStorage.setItem('priceCount',JSON.stringify(priceCount));
+        var bidSuccess = _.find(priceCount,function(num){return num.count == 1});
+        var sucess = _.find(people_list,function(num){return num.price == bidSuccess.bidPrice});
+        console.log(sucess);
+        localStorage.setItem('sucess',JSON.stringify(sucess));
+        $scope.sucessName = sucess.name;
+        $scope.sucessPhone = sucess.phone;
+        $scope.sucessPrice = sucess.price
     });
