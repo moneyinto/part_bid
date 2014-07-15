bc08fec01ffe919271b423f2a768b0ae7c3f38bd
your_name <your_email>
2014-07-15 21:27:04 +0800

    竞价统计页面显示

--- app/scripts/controllers/bidding_count.js
+++ app/scripts/controllers/bidding_count.js
@@ -7,8 +7,38 @@ angular.module('card1App')
             'AngularJS',
             'Karma'
         ];
-        $scope.back_to_bidding_list = function() {
+        $scope.back_to_bidding_list = function () {
             $location.path('bidding_list');
+        };
+
+        var bidList = JSON.parse(localStorage.getItem('bidList'));
+        var bidInformation = bidList[0].bidInformation;
+        var information = _.sortBy(bidInformation, function (num) {
+            return num.bidPrice
+        });
+//        var price = _.groupBy(information,function(num){return num.bidPrice});
+        var priceCount = [];
+        var n = 1;
+        for (var i = 0; i < information.length; i++) {
+            if (i == information.length - 1) {
+                if (information[i].bidPrice == information[i - 1].bidPrice) {
+                    priceCount.push({'bidPrice': information[i].bidPrice, 'count': n});
+                    break;
+                }
+                else {
+                    priceCount.push({'bidPrice': information[i].bidPrice, 'count': n});
+                }
+            }
+            else {
+                if (information[i].bidPrice == information[i + 1].bidPrice) {
+                    n = n + 1;
+                }
+                else {
+                    priceCount.push({'bidPrice': information[i].bidPrice, 'count': n});
+                    n = 1;
+                }
+            }
         }
+        $scope.priceCount = priceCount;
 
     });
