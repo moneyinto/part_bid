045d8e284c0bfdd7c03cae2289c06d62db2ac8d8
your_name <your_email>
2014-07-16 09:42:41 +0800

    三秒后显示信息

--- app/scripts/controllers/bidding_count.js
+++ app/scripts/controllers/bidding_count.js
@@ -17,9 +17,16 @@ angular.module('card1App')
         var priceCount = JSON.parse(localStorage.getItem('priceCount'));
         var sucess = JSON.parse(localStorage.getItem('sucess'));
         $scope.priceCount = priceCount;
-        $scope.sucessName = sucess.name;
-        $scope.sucessPhone = sucess.phone;
-        $scope.sucessPrice = sucess.price;
+        setTimeout(function(){
+            $scope.$apply(function(){
+                $scope.bidResult = "竞价结果：";
+                $scope.phone = "电话：";
+                $scope.price = "竞价：￥";
+                $scope.sucessName = sucess.name;
+                $scope.sucessPhone = sucess.phone;
+                $scope.sucessPrice = sucess.price;
+            });
+        },3000);
         $scope.bidName = bidList[0].name;
         if(bidInformation.length){
             $scope.bidCount = bidInformation.length;
--- app/scripts/controllers/bidding_result.js
+++ app/scripts/controllers/bidding_result.js
@@ -55,9 +55,17 @@ angular.module('card1App')
         var sucess = _.find(people_list,function(num){return num.price == bidSuccess.bidPrice});
         console.log(sucess);
         localStorage.setItem('sucess',JSON.stringify(sucess));
-        $scope.sucessName = sucess.name;
-        $scope.sucessPhone = sucess.phone;
-        $scope.sucessPrice = sucess.price;
+        setTimeout(function(){
+            $scope.$apply(function(){
+                $scope.bidResult = "竞价结果：";
+                $scope.phone = "电话：";
+                $scope.price = "竞价：￥";
+                $scope.sucessName = sucess.name;
+                $scope.sucessPhone = sucess.phone;
+                $scope.sucessPrice = sucess.price;
+            });
+        },3000);
+
         $scope.bidName = bidList[0].name;
         if(bidInformation.length){
             $scope.bidCount = bidInformation.length;
